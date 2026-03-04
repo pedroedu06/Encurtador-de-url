34773e80a29a4cd60bb96e756bee414e4f23e498
pedroedu06 <pedro.nascimento182501@gmail.com>
2026-03-03 21:24:18 -0300

    postgres funcionando

--- src/controller/urlshortener.ts
+++ src/controller/urlshortener.ts
@@ -1,16 +1,31 @@
-import base62 from "base62";
 import { query } from "../postgres.js";
 
 
-const encurtaURL = (url: string) => {
+const encurtaURL = async (url: string) => {
     try {
         new URL(url);
     } catch {
-        throw { code: 404, message: "error ao achar a url. "}
+        throw { code: 404, message: "error ao achar a url. " }
     }
 
-    const shortcode = nanoid(6);
+    async function insertlink(url: string) {
+        try {
+            const result = await query(
+                `INSERT INTO links (long_url)
+                 VALUES ($1)
+                 RETURNING id;                 
+                `, [url]
+            );
 
+            const id = result.rows[0].id;
+            console.log("link inserido com sucesso")
+            return id;          
+        } catch {
+            console.error("ERROR 422, nao foi possivel inserir no banco de dados!")
+        }
+    };
+    
+    return await insertlink(url);
 }
 
 export default encurtaURL;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
-import express from 'express';
+import express from "express";
+import encurtaURL from "./controller/urlshortener.js";
 
 const app = express();
 app.use(express.json());
 
 
-app.post('/sendurl', (req, res) => {
-    console.log(req.body);
-    res.json({message: "ok"})
+app.post('/sendurl', async (req, res) => {
+    try {
+        const urlid = await encurtaURL(req.body.url);
+        console.log(urlid)
+        res.json({ message: "ok" })
+    } catch (err: any) {
+        console.error(err)
+        res.status(err?.code || 500).json({ error: err?.message || "internal error" })
+    }
 })
 
 app.listen(3000, () => {
